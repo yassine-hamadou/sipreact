eb973cb74435916287b6c05de6c5779c66fd6eaf
yassine <yassinehamadou1@gmail.com>
2022-09-24 19:38:39 +0000

    Uploading work

---
+++ src/_metronic/layout/components/aside/AsideMenuMain.tsx
@@ -0,0 +1,18 @@
+/* eslint-disable react/jsx-no-target-blank */
+import React from 'react'
+import {useIntl} from 'react-intl'
+import {AsideMenuItem} from './AsideMenuItem'
+export function AsideMenuMain() {
+  const intl = useIntl()
+
+  return (
+    <>
+      <AsideMenuItem
+        to='/apps/user-management/users'
+        icon='/media/icons/duotune/general/gen051.svg'
+        title='User management'
+        fontIcon='bi-layers'
+      />
+    </>
+  )
+}
